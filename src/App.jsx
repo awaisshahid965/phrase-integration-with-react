4607ddc821a8f5458f9ea449b8d7d579660b3a57
Muhammad Awais <awaisshahid965@gmail.com>
2023-04-03 16:28:09 -0700

    added package react-intl-phrase + configure

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { FormattedMessage, injectIntl } from 'react-intl-phraseapp';
+
+
+function App(props) {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>{props.translate("welcome.headline")}</h2>
+      </div>
+      <p className="App-intro">
+        <FormattedMessage
+          id="welcome.intro"
+          defaultMessage={`We hope this example will help you integrate PhraseApp into your react app using react-intl`}
+        />
+      </p>
+      <p>{props.formatMessage({ id: "welcome.text" })}</p>
+    </div>
+  );
+}
+
+export default injectIntl(App);
