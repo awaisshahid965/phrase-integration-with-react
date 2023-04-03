73c462452cf7dda45686d0d4462dd3d4ef5319f4
Muhammad Awais <awaisshahid965@gmail.com>
2023-04-03 16:29:04 -0700

    missing file from last commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {IntlProvider} from 'react-intl';
+import {initializePhraseAppEditor} from 'react-intl-phraseapp'
+
+var config = {
+  projectId: '554fe040bdb0fad1f9241345900a9511',
+  phraseEnabled: true,
+  prefix: '[[__',
+  suffix: '__]]'
+}
+
+initializePhraseAppEditor(config);
+
+const messages = {
+  'welcome.headline': 'This headline is visible when "phraseEnabled" config option is set to false'
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <IntlProvider locale="en" messages={messages}>
     <App />
+    </IntlProvider>
   </React.StrictMode>
 );
 
